33bb604249aa487c1eed6db3f3d0cd1baf7ff072
Seth Gibson <seth.gibson@intel.com>
2012-12-21 16:00:43 -0800

    Perceptual Computing SDK + MSAFluid, it had to happen -sG

--- MSAFluidPXC/src/testApp.cpp
+++ MSAFluidPXC/src/testApp.cpp
@@ -1,61 +1,110 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void testApp::setup()
+{
+	//Setup PXC
+	mSession = PXCUPipeline_Init((PXCUPipeline)(PXCU_PIPELINE_COLOR_VGA|PXCU_PIPELINE_GESTURE));
+	if(!mSession)
+		ofExit();
+	if(!PXCUPipeline_QueryRGBSize(mSession,&mcw,&mch))
+		ofExit();
+	mRgbMap = new unsigned char[mcw*mch*4];
+	mRgbTex = ofTexture();
+	mRgbTex.allocate(mcw,mch,GL_RGBA);
+	
+	//Setup fluid	
+	mSolver.setup(160,120);
+	mSolver.enableRGB(true).setColorDiffusion(0).setDeltaT(0.5).setFadeSpeed(0.01).setVisc(0.0002);
+	mDrawer.setup(&mSolver);
+	mDrawer.enableAlpha(true);
+	mDrawer.useAdditiveBlending = true;
 
+	//Setup ofx
+	ofSetWindowShape(mcw,mch);
+	ofEnableAlphaBlending();	
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void testApp::update()
+{
+	if(PXCUPipeline_AcquireFrame(mSession, true))
+	{
+		if(PXCUPipeline_QueryRGB(mSession, mRgbMap))
+		{
+			mRgbTex.loadData(mRgbMap,mcw,mch,GL_RGBA);
+			if(PXCUPipeline_QueryGeoNode(mSession, PXCGesture::GeoNode::LABEL_BODY_HAND_PRIMARY, &mNode))
+			{
+				mPHand1 = mPHand0;
+				mPHand0.set(ofLerp(mPHand0.x,mNode.positionImage.x,0.5f),
+							ofLerp(mPHand0.y,mNode.positionImage.y,0.5f));
+				
+				addToSolver(&mSolver,
+							msa::Vec2f(mPHand1.x,mPHand1.y),
+							msa::Color(255,0,0),
+							msa::Vec2f(mPHand0.x-mPHand1.x,mPHand0.y-mPHand1.y),
+							mNode.openness);
+			}
+			if(PXCUPipeline_QueryGeoNode(mSession, PXCGesture::GeoNode::LABEL_BODY_HAND_SECONDARY, &mNode))
+			{
+				mSHand1 = mSHand0;
+				mSHand0.set(ofLerp(mSHand0.x,mNode.positionImage.x,0.5f),
+							ofLerp(mSHand0.y,mNode.positionImage.y,0.5f));
 
+				addToSolver(&mSolver,
+							msa::Vec2f(mSHand1.x,mSHand1.y),
+							msa::Color(0,0,255),
+							msa::Vec2f(mSHand0.x-mSHand1.x,mSHand0.y-mSHand1.y),
+							mNode.openness);
+			}
+		}
+		mSolver.update();
+		PXCUPipeline_ReleaseFrame(mSession);
+	}
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-
+void testApp::draw()
+{
+	ofClear(0);
+	mRgbTex.draw(0,0,mcw,mch);
+	mDrawer.draw(0,0,mcw,mch);
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
+void testApp::addDensity(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Color pColor, int pSize)
+{
+	msa::Vec2f newPos;
+	for(int i=0;i<pSize;i++)
+	{
+		float rx = ofRandom(-pSize,pSize);
+		float ry = ofRandom(-pSize,pSize);
+		newPos = msa::Vec2f((pPos.x+rx)/320.f,(pPos.y+ry)/240.f);
+		pSolver->addColorAtPos(newPos, pColor);
+	}
 }
 
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
+void testApp::addVelocity(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Vec2f pForce, int pSize)
+{
+	msa::Vec2f newPos;
+	for(int i=0;i<pSize;i++)
+	{
+		float rx = ofRandom(-pSize,pSize);
+		float ry = ofRandom(-pSize,pSize);
+		newPos = msa::Vec2f((pPos.x+rx)/320.f,(pPos.y+ry)/240.f);
+		pSolver->addForceAtPos(newPos, pForce);
+	}
 }
 
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void testApp::addToSolver(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Color pColor, msa::Vec2f pForce, int pSize)
+{
+	if(pSize<10)
+	{
+		addVelocity(pSolver, pPos, pForce, 100);
+	}
+	else
+	{
+		int fSize = (int)ofMap(pSize,10,100,5,10);
+		addDensity(pSolver, pPos, pColor, fSize);
+		addVelocity(pSolver, pPos, pForce, fSize);
+	}
 }
--- MSAFluidPXC/src/testApp.h
+++ MSAFluidPXC/src/testApp.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "ofMain.h"
+#include "MSAFluid.h"
+#include "MSAFluidSolver.h"
+#include "MSAFluidDrawerGl-OF.h"
+#include "pxcupipeline.h"
 
 class testApp : public ofBaseApp{
 
@@ -8,15 +12,19 @@ class testApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+		void addDensity(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Color pColor, int pSize);
+		void addVelocity(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Vec2f pForce, int pSize);
+		void addToSolver(msa::fluid::Solver *pSolver, msa::Vec2f pPos, msa::Color pColor, msa::Vec2f pForce, int pSize);
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+		int mcw,mch;
+		unsigned char *mRgbMap;
+		ofTexture mRgbTex;
+		PXCUPipeline_Instance mSession;
+		PXCGesture::GeoNode mNode;
+
+		ofPoint mPHand0, mPHand1;
+		ofPoint mSHand0, mSHand1;
+		msa::fluid::Solver mSolver;
+		msa::fluid::DrawerGl mDrawer;
+
 };
