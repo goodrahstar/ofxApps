52272a34e714561b357ef413278d86f0c22fc58c
Seth Gibson <seth.gibson1@gmail.com>
2013-05-31 18:45:46 -0700

    touching fingertracking (quit being weird git) -sG

--- FaceTracking/src/testApp.h
+++ FaceTracking/src/testApp.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "ofMain.h"
+#include "util_pipeline.h"
 
 class testApp : public ofBaseApp
 {
 public:
-		void setup();
-		void update();
-		void draw();
+	void setup();
+	void update();
+	void draw();
+
+private:
+	UtilPipeline mSession;
 };
