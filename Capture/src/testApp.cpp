569426f2c9745c343016a98e560ed2ce8c4067e0
Seth Gibson <seth.gibson1@gmail.com>
2013-06-01 11:28:05 -0700

    cleanup -sG

--- Capture/src/testApp.cpp
+++ Capture/src/testApp.cpp
@@ -1,61 +1,68 @@
 #include "testApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
-
+void testApp::setup()
+{
+	ofSetLogLevel(OF_LOG_NOTICE);
+	ofSetWindowShape(640,240);
+	mSession.EnableImage(PXCImage::COLOR_FORMAT_RGB24);
+	mSession.EnableImage(PXCImage::COLOR_FORMAT_DEPTH);
+	mSession.EnableGesture();
+	if(!mSession.Init())
+	{
+		ofLogNotice() << "Unable Starting Session" << endl;
+		return;
+	}
+	
+	if(mSession.QueryImageSize(PXCImage::IMAGE_TYPE_COLOR, mRGBW, mRGBH))
+		mRGBTexture.allocate(mRGBW,mRGBH,GL_RGB);
+	else
+		ofLogNotice() << "Unable to Query RGB Map Size" << endl;
+	if(mSession.QueryImageSize(PXCImage::IMAGE_TYPE_DEPTH, mLW, mLH))
+		mLabelTexture.allocate(mLW,mLH,GL_LUMINANCE);
+	else
+		ofLogNotice() << "Unable to Query Depth Map Size" << endl;
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void testApp::update()
+{
+	if(mSession.AcquireFrame(false))
+	{
+		mImage = mSession.QueryImage(PXCImage::IMAGE_TYPE_COLOR);
+		if(mImage)
+		{
+			if(mImage->AcquireAccess(PXCImage::ACCESS_READ, &mImageData)>=PXC_STATUS_NO_ERROR)
+			{
+				mRGBTexture.loadData(mImageData.planes[0], mRGBW, mRGBH,GL_RGB);
+				mImage->ReleaseAccess(&mImageData);
+			}
+			else
+				ofLogNotice()
+		}
+		// Get Label (Blob) Map
 
+		if(mSession.QueryGesture()->QueryBlobImage(PXCGesture::Blob::LABEL_SCENE, 0, &mImage)>=PXC_STATUS_NO_ERROR)
+		{
+			if(mImage->AcquireAccess(PXCImage::ACCESS_READ, &mImageData)>=PXC_STATUS_NO_ERROR)
+			{
+				mLabelTexture.loadData(mImageData.planes[0], mLW, mLH, GL_LUMINANCE);
+				mImage->ReleaseAccess(&mImageData);
+			}			
+		}
+		mSession.ReleaseFrame();
+	}
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
-
+void testApp::draw()
+{
+	//ofSetColor(ofColor::white);
+	//mRGBTexture.draw(0,0,320,240);
+	mLabelTexture.draw(320,0,320,240);
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+void testApp::exit()
+{
+	mSession.Close();
 }
--- Capture/src/testApp.h
+++ Capture/src/testApp.h
@@ -1,22 +1,23 @@
 #pragma once
 
 #include "ofMain.h"
+#include "util_pipeline.h"
 
-class testApp : public ofBaseApp{
+class testApp : public ofBaseApp
+{
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+private:
+	pxcU32 mRGBW, mRGBH, mLW, mLH;
+	PXCGesture *mGesture;
+	PXCImage *mImage;
+	PXCImage::ImageData mImageData;
+	UtilPipeline mSession;
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	ofTexture mRGBTexture;
+	ofTexture mLabelTexture;
 };
